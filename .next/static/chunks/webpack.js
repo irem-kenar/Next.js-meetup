9227a872d990c099b58a755b44d771b82eb4caf2
Irem Kenar <ikenar@kazandirio-dev.com>
2022-01-23 21:50:19 +0300

    Added getStaticPaths

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8db917155c9b4216ce43";
+/******/ 	var hotCurrentHash = "d6d0aa5bd9a9e5041462";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
