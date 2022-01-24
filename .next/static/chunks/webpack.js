445417459c04e8ad5554b081b35461e19a381229
Irem Kenar <ikenar@kazandirio-dev.com>
2022-01-24 15:16:13 +0300

    Configured meetupdetails

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "240a0aa14498de6471aa";
+/******/ 	var hotCurrentHash = "cdb80cbdcbd1f7711e47";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,47 +1,84 @@
-
+import { MongoClient, ObjectId } from "mongodb";
 import MeetupDetail from "../../components/meetups/meetupDetail";
 
-function MeetupDetails() {
+function MeetupDetails(props) {
     return (
         <MeetupDetail
-            image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
-            title="First Meetup"
-            address="Some Street 5, Some City"
-            description="Meetup description" />
+            image={props.meetupData.image}
+            title={props.meetupData.title}
+            address={props.meetupData.address}
+            description={props.meetupData.description}
+        // image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+        // title="First Meetup"
+        // address="Some Street 5, Some City"
+        // description="Meetup description" 
+        />
     )
 }
 
 export async function getStaticPaths() {
+
+    const client = await MongoClient.connect('mongodb+srv://bUG9e78ulpLnX9OP:bUG9e78ulpLnX9OP@cluster0.ojaqa.mongodb.net/meetups?retryWrites=true&w=majority');
+
+    const db = client.db();
+    const meetupsCollection = db.collection('meetups');
+
+    const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+    client.close();
+
     return {
         fallback: false,
-        paths: [
-            {
-                params: {
-                    meetupId: 'm1'
-                }
-            },
-            {
-                params: {
-                    meetupId: 'm2'
-                }
-            }
-        ]
+        //generating paths dynamically
+        paths: meetups.map(meetup => ({ params: { meetupId: meetup._id.toString() } }))
+        // [
+        //     {
+        //         params: {
+        //             meetupId: 'm1'
+        //         }
+        //     },
+        //     {
+        //         params: {
+        //             meetupId: 'm2'
+        //         }
+        //     }
+        // ]
     }
 }
 
 export async function getStaticProps(context) {
     // fetch data for a single meetup
     const meetupId = context.params.meetupId;
-    console.log(meetupId);
+    // fetch a single meetup dynamically
+    const client = await MongoClient.connect('mongodb+srv://bUG9e78ulpLnX9OP:bUG9e78ulpLnX9OP@cluster0.ojaqa.mongodb.net/meetups?retryWrites=true&w=majority');
+
+    const db = client.db();
+    const meetupsCollection = db.collection('meetups');
+
+    const selectedMeetup = await meetupsCollection.findOne({ _id: ObjectId(meetupId) });
+
+    client.close();
+
     return {
         props: {
+            //dynamically and need to convert _id back to string not to get an error
             meetupData: {
-                image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-                id: meetupId,
-                title: 'First Meetup',
-                address: 'Some Street 5, Some City',
-                description: 'Meetup description'
-            },
+                id: selectedMeetup._id.toString(),
+                title: selectedMeetup.title,
+                address: selectedMeetup.address,
+                image: selectedMeetup.image,
+                description: selectedMeetup.description
+            }
+            //dynamically 
+            //meetupData: selectedMeetup
+            //Hardcoded
+            // {
+            //     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+            //     id: meetupId,
+            //     title: 'First Meetup',
+            //     address: 'Some Street 5, Some City',
+            //     description: 'Meetup description'
+            // },
         },
     }
 }
